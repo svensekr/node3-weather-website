7baa1c3b61fcffcff5481b58000ba25fded8dc64
Rene Svensek <rsvensek@ptm-edv.at>
2019-06-11 09:49:54 +0200

    Add celsius and wind to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,15 +2,15 @@ const request = require('request')
 
 const forecast = (latitude,longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/d3804c2e39bb7ff822d4b9269169cc8b/' + latitude + ',' + longitude
-
+    
     request({ url, json: true }, (error, {body}) => {
-
+        var celzija = (body.currently.temperature - 32) * 5 / 9;
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature  + ' degrees out. ' + 'There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + celzija  + ' celsius. ' + 'There is a ' + body.currently.precipProbability + '% chance of rain. And wind is currently blowing at ' + body.currently.windSpeed + ' per hour. ')
             }
     })
 }
