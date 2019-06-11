d0882b01900531add1ae0843d91d97d810594c4b
Rene Svensek <rsvensek@ptm-edv.at>
2019-06-11 11:04:06 +0200

    Forecast temperature to 2 decimals

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,12 +5,13 @@ const forecast = (latitude,longitude, callback) => {
     
     request({ url, json: true }, (error, {body}) => {
         var celzija = (body.currently.temperature - 32) * 5 / 9;
+        result = Math.round(celzija * 100) / 100;
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + celzija  + ' celsius. ' + 'There is a ' + body.currently.precipProbability + '% chance of rain. And wind is currently blowing at ' + body.currently.windSpeed + ' per hour. ')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + result  + ' celsius. ' + 'There is a ' + body.currently.precipProbability + '% chance of rain. And wind is currently blowing at ' + body.currently.windSpeed + ' per hour. ')
             }
     })
 }
